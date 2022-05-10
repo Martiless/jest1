722ac97df7cbdb316f540f9219fdbdbc1577e677
Martina Martin <marti.law@gmail.com>
2022-05-10 10:48:05 +0000

    Add choices to array

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const exp = require("constants");
 const { game } = require("../game");
 
 
@@ -16,14 +17,17 @@ beforeAll(() => {
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
-    })
+    });
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
-    })
+    });
     test("playerMoves key exists", () => {
         expect("playerMoves" in game).toBe(true);
-    })
-    test("choice key exists", () => {
-        expect("choice" in game).toBe(true);
-    })
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1","button2","button3","button4"]);
+    });
 })
